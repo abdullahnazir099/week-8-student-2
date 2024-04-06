a5dbdb1291bee164a5a2b3396b5b3bee1aa6e661
abdullal <abdullahnazir.099@gmail.com>
2024-04-06 06:05:30 +0500

    first

--- netlify/functions/updateBook.js
+++ netlify/functions/updateBook.js
@@ -1,6 +1,18 @@
 const mysql = require('mysql2/promise');
 
 exports.handler = async (event, context) => {
+    /* 
+  if (!context.clientContext || !context.clientContext.user) {
+    return {
+    statusCode: 401,
+    body: JSON.stringify({ error: 'You must be logged in.' }),
+    };
+    }
+    
+    const user = context.clientContext.user;
+  
+    console.log('Authenticated user:', user);
+*/
   try {
     const { id, title, author, isbn, published_year, genre } = JSON.parse(event.body);
 
